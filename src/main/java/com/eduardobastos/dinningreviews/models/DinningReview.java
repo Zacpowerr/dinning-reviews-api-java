9aee0b361c103fbb750e8cfd4b7d1fd5e37f71fc
Eduardo Bastos <e_kempfer@hotmail.com>
2025-06-13 11:13:35 -0600

    Refactor entity classes to enforce unique ID generation strategy for DinningReview, Restaurant, and User models

--- src/main/java/com/eduardobastos/dinningreviews/models/DinningReview.java
+++ src/main/java/com/eduardobastos/dinningreviews/models/DinningReview.java
@@ -3,6 +3,7 @@ package com.eduardobastos.dinningreviews.models;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -11,7 +12,8 @@ import jakarta.persistence.Table;
 public class DinningReview {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
     private Integer id;
     @Column(name = "user_name")
     private String userName;
--- src/main/java/com/eduardobastos/dinningreviews/models/Restaurant.java
+++ src/main/java/com/eduardobastos/dinningreviews/models/Restaurant.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -15,7 +16,8 @@ import jakarta.persistence.Table;
 public class Restaurant {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
     private Integer id;
     @Column(name = "name")
     private String name;
