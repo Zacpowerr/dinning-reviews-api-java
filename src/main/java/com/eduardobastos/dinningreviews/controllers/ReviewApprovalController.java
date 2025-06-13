b88198977145ac3d6e2d6f192b9690ac49150be8
Eduardo Bastos <e_kempfer@hotmail.com>
2025-06-13 11:13:22 -0600

    Implement review approval and denial endpoints with error handling

--- src/main/java/com/eduardobastos/dinningreviews/controllers/ReviewApprovalController.java
+++ src/main/java/com/eduardobastos/dinningreviews/controllers/ReviewApprovalController.java
@@ -1,10 +1,16 @@
 package com.eduardobastos.dinningreviews.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.eduardobastos.dinningreviews.models.DinningReview;
 import com.eduardobastos.dinningreviews.models.DinningReviewStatus;
@@ -32,4 +38,36 @@ public class ReviewApprovalController {
         return createdReviews;
     }
 
+    @PostMapping("/{reviewId}/approve")
+    public DinningReview approveReview(@PathVariable Integer reviewId) {
+        Optional<DinningReview> reviewToUpdateOptional = drr.findById(reviewId);
+        if (!reviewToUpdateOptional.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Review does not exist");
+        }
+        DinningReview reviewToUpdate = reviewToUpdateOptional.get();
+        reviewToUpdate.setStatus(DinningReviewStatus.APPROVED);
+        DinningReview updatedReview = drr.save(reviewToUpdate);
+        return updatedReview;
+    }
+
+    @PostMapping("/{reviewId}/deny")
+    public DinningReview denyReview(@PathVariable Integer reviewId) {
+        Optional<DinningReview> reviewToUpdateOptional = drr.findById(reviewId);
+        if (!reviewToUpdateOptional.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Review does not exist");
+        }
+        DinningReview reviewToUpdate = reviewToUpdateOptional.get();
+        reviewToUpdate.setStatus(DinningReviewStatus.DENIED);
+        DinningReview updatedReview = drr.save(reviewToUpdate);
+        return updatedReview;
+    }
+
+    // TODO: Remove this for final commit
+    // COMMENT: Created this endpoint to easily clean up database
+    @DeleteMapping("/purge")
+    public void purgeData() {
+        drr.deleteAll();
+        rr.deleteAll();
+        ur.deleteAll();
+    }
 }
